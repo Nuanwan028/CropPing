fec3e6cf075cc02a8e0cb96d973c5d3ee94cc193
kmj <nuanwan_w@cmu.ac.th>
2026-04-28 23:20:40 +0700

    add new endpoint

--- src/main/java/cropping/controller/WebhookController.java
+++ src/main/java/cropping/controller/WebhookController.java
@@ -1,10 +1,7 @@
 package cropping.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import cropping.dto.Event;
 import cropping.dto.LineWebhookRequest;
 import cropping.service.CropService;
@@ -132,5 +129,10 @@ public class WebhookController {
         }
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "ok";
     }
 }
