c0059bfb0a485bcbf53fa9076ec98efffa42d11b
kmj <nuanwan_w@cmu.ac.th>
2026-06-27 19:48:18 +0700

    Fix build - remove okhttp-ws, use jakarta annotations

--- src/main/java/cropping/service/DiscordGatewayService.java
+++ src/main/java/cropping/service/DiscordGatewayService.java
@@ -10,8 +10,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import java.util.concurrent.TimeUnit;
 
 /**
